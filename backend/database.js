45500be06edce579c8b4ee258a4aecd391d8566e
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-02-23 22:33:00 -0800

    Create initial DB interface

--- backend/database.js
+++ backend/database.js
@@ -5,15 +5,27 @@ mongoose.set('strictQuery', true);
 //Change connection later once Heroku is set up
 mongoose.connect("mongodb://localhost/rankings"); 
 
-run()
-async function run() {
+async function insertIntoDatabase() {
     try {
         const record = await leaderboards.create({
-            name: "Hali", 
-            score: 5
+            name: "Raven", 
+            score: 7
         }); 
         console.log(record);
+        console.log("The record has been added!");
     } catch (e) {
         console.log(e.message);
+    }
+    finally {
+        mongoose.disconnect(() => {
+            console.log("The database has been successfully disconnected");
+        });
+    }
+}
+
+//add CRUD operations when leaderboard structure is defined
+module.exports = db_interface = {
+    insert: () => { //add arg later
+        insertIntoDatabase();
     }
 }
