4ab519969911625c0c002061e515812b539b15dd
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-03-16 14:12:45 -0700

    Set-up testing environment and test user join

---
+++ testing/users.test.js
@@ -0,0 +1,16 @@
+const {
+    joinUser,
+    getCurrentUser,
+    leaveUser,
+    getUsersInRoom,
+} = require('../backend/users');
+
+test("User object with correct values", () => {
+    const userTest = {
+        id: 5,
+        username: "Hali",
+        room: "Room 1"
+    }
+    expect(joinUser(5, "Hali", "Room 1")).toEqual(userTest)
+    expect(joinUser(5, "Joshua", "Room 1")).not.toEqual(userTest)
+})
